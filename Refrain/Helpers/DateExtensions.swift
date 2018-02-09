c18c2533ce5a1e4bcb984818047514d8e51cfd7a
Kyle <kyle@kyles-macbook-pro.local>
2018-02-08 20:22:17 -0500

    Add User object (to send to server)

--- Refrain/Helpers/DateExtensions.swift
+++ Refrain/Helpers/DateExtensions.swift
@@ -26,4 +26,28 @@ extension Date {
         print(offset/(60*60))
         return Date(timeIntervalSince1970: (60*60*17)-(offset))
     }
+    
+    
+    
+    //MARK: - Time Integer
+    // Time Integer is a number between 0 and 1439, representing the current minute in the day. (There are 1440 minutes in a day). This is how a timestamp is represented in the database.
+    init?(fromTimeInteger integer: Int) {
+        let minutes = integer % 60
+        let hour = (integer-minutes)/60
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm"
+        
+        if let date = formatter.date(from: "\(hour):\(minutes)") {
+            self = date
+        } else {
+            return nil
+        }
+    }
+    
+    func toTimeInteger() -> Int {
+        let hour = Calendar.current.component(.hour, from: self)
+        let mins = Calendar.current.component(.minute, from: self)
+        return (hour*60)+mins
+    }
 }
