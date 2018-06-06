e1755f6802e04c5b194ad1b784c2a4be1f890eb5
Kyle Genoe <kyle@genoe.ca>
2018-06-06 16:08:17 -0400

    Refactor Main table delegate & datasource into MainTableViewDelegate

---
+++ Refrain/View Controllers/Main/MainTableViewDelegate.swift
@@ -0,0 +1,142 @@
+//
+//  MainTableViewDelegate.swift
+//  Refrain
+//
+//  Created by Kyle Genoe on 2018-06-06.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewDelegate: NSObject {
+    
+    var structure: MainTableViewStructure
+    
+    var collectionSelectedHandler: ((BlockingCollection) -> Void)?
+    
+    var newCollectionSelectedHandler: (() -> Void)?
+    
+    var schedulesSelectedHandler: (() -> Void)?
+    
+    var settingsSelectedHandler: (() -> Void)?
+    
+    init(structure: MainTableViewStructure) {
+        self.structure = structure
+        super.init()
+    }
+}
+
+extension MainTableViewDelegate: UITableViewDelegate {
+    
+    // MARK: - Section Headers
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch structure.sectionType(for: section) {
+        case .Schedules, .Settings: return 40
+        default: return 0
+        }
+    }
+}
+
+
+extension MainTableViewDelegate: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return structure.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return structure.rowCount(for: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch structure.rowType(for: indexPath) {
+        case .DefaultBlockingCollectionsHeader:
+            return HeaderTableViewCell(title: "Default Collections")
+        case .DefaultBlockingCollections(let i):
+            let collection = BlockingCollectionStore.shared.collections.filter{ $0.isDefault }[i]
+            return BlockingCollectionCell(blockingCollection: collection)
+        case .UserBlockingCollectionsHeader:
+            return HeaderTableViewCell(title: "Custom Collections")
+        case .UserBlockingCollections(let i):
+            let collection = BlockingCollectionStore.shared.collections.filter{ !$0.isDefault }[i]
+            return BlockingCollectionCell(blockingCollection: collection)
+        case .NewUserBlockingCollection:
+            let cell = HeaderTableViewCell(title: "New Blocking Collection")
+            cell.titleLabel.textAlignment = .center
+            cell.titleLabel.textColor = UIColor(named: "Orange")
+            return cell
+        case .Schedules:
+            return ItemTableViewCell(text: "Schedules")
+        case .Settings:
+            return ItemTableViewCell(text: "Settings")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch structure.rowType(for: indexPath) {
+        case .DefaultBlockingCollections(let i):
+            let collection = BlockingCollectionStore.shared.collections.filter{ $0.isDefault }[i]
+            collectionSelectedHandler?(collection)
+        case .UserBlockingCollections(let i):
+            let collection = BlockingCollectionStore.shared.collections.filter{ !$0.isDefault }[i]
+            collectionSelectedHandler?(collection)
+        case .NewUserBlockingCollection:
+            newCollectionSelectedHandler?()
+        case .Schedules:
+            schedulesSelectedHandler?()
+        case .Settings:
+            settingsSelectedHandler?()
+        default:
+            break
+        }
+    }
+    
+    
+    /// Conditionally disable selection for some table view rows
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        
+        switch structure.rowType(for: indexPath) {
+        case .DefaultBlockingCollections(_), .UserBlockingCollections(_), .NewUserBlockingCollection, .Schedules, .Settings:
+            return indexPath
+        default:
+            return nil
+        }
+    }
+    
+    /// Conditionally enable deletion for user created blocking collections
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        switch structure.rowType(for: indexPath) {
+        case .UserBlockingCollections(_): return true
+        default: return false
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch (editingStyle, structure.rowType(for: indexPath)) {
+        case (.delete, .UserBlockingCollections(let i)):
+            
+            // delete the collection in the data store
+            let collections = BlockingCollectionStore.shared.collections
+            let userCreatedCollections = collections.filter{ $0.isDefault == false }
+            let collectionToDelete = userCreatedCollections[i]
+            BlockingCollectionStore.shared.delete(collectionToDelete)
+            
+            // refresh the table view structure
+            let defaultCount = collections.filter{ $0.isDefault }.count
+            let userCount = collections.filter{ !$0.isDefault }.count
+            structure = MainTableViewStructure(defaultCollectionsCount: defaultCount, userCollectionsCount: userCount)
+            
+            // delete the table view cell
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        default: break
+        }
+    }
+}
--- Refrain/View Controllers/Main/MainViewController.swift
+++ Refrain/View Controllers/Main/MainViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    private var tableViewStructure: MainTableViewStructure!
-
+    private var tableViewDelegate: MainTableViewDelegate!
+    
     @IBOutlet private weak var tableView: UITableView!
 
     override func viewWillAppear(_ animated: Bool) {
@@ -27,10 +27,20 @@ class MainViewController: UIViewController {
     
     private func setupView() {
         
-        tableViewStructure = updateTableViewStructure()
-
-        tableView.delegate = self
-        tableView.dataSource = self
+        // Build TableViewStructure
+        let collections = BlockingCollectionStore.shared.collections
+        let defaultCount = collections.filter{ $0.isDefault }.count
+        let userCount = collections.filter{ !$0.isDefault }.count
+        let tableViewStructure = MainTableViewStructure(defaultCollectionsCount: defaultCount, userCollectionsCount: userCount)
+        
+        // Build TableView Delegate/Datasource
+        tableViewDelegate = MainTableViewDelegate(structure: tableViewStructure)
+        tableViewDelegate.collectionSelectedHandler = pushBlockingCollectionView
+        tableViewDelegate.newCollectionSelectedHandler = presentCreateBlockingCollectionAlert
+        tableViewDelegate.schedulesSelectedHandler = pushSchedulesView
+        tableViewDelegate.settingsSelectedHandler = pushSettingsView
+        tableView.delegate = tableViewDelegate
+        tableView.dataSource = tableViewDelegate
         
         tableView.indexPathsForSelectedRows?.forEach {
             tableView.deselectRow(at: $0, animated: false)
@@ -57,14 +67,6 @@ class MainViewController: UIViewController {
         
         let gradient = BackgroundGradientLayer(frame: view.bounds)
         view.layer.addSublayer(gradient)
-    }
-    
-    
-    private func updateTableViewStructure() -> MainTableViewStructure {
-        let collections = BlockingCollectionStore.shared.collections
-        let defaultCount = collections.filter{ $0.isDefault }.count
-        let userCount = collections.filter{ !$0.isDefault }.count
-        return MainTableViewStructure(defaultCollectionsCount: defaultCount, userCollectionsCount: userCount)
     }
     
     
@@ -121,118 +123,6 @@ class MainViewController: UIViewController {
             alert.actions[1].isEnabled = false
         } else {
             alert.actions[1].isEnabled = true
-        }
-    }
-}
-
-
-
-//MARK: - UITableViewDelegate & UITableViewDataSource
-extension MainViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return tableViewStructure.sections.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return tableViewStructure.rowCount(for: section)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        switch tableViewStructure.rowType(for: indexPath) {
-        case .DefaultBlockingCollectionsHeader:
-            return HeaderTableViewCell(title: "Default Collections")
-        case .DefaultBlockingCollections(let i):
-            let collection = BlockingCollectionStore.shared.collections.filter{ $0.isDefault }[i]
-            return BlockingCollectionCell(blockingCollection: collection)
-        case .UserBlockingCollectionsHeader:
-            return HeaderTableViewCell(title: "Custom Collections")
-        case .UserBlockingCollections(let i):
-            let collection = BlockingCollectionStore.shared.collections.filter{ !$0.isDefault }[i]
-            return BlockingCollectionCell(blockingCollection: collection)
-        case .NewUserBlockingCollection:
-            let cell = HeaderTableViewCell(title: "New Blocking Collection")
-            cell.titleLabel.textAlignment = .center
-            cell.titleLabel.textColor = UIColor(named: "Orange")
-            return cell
-        case .Schedules:
-            return ItemTableViewCell(text: "Schedules")
-        case .Settings:
-            return ItemTableViewCell(text: "Settings")
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch tableViewStructure.rowType(for: indexPath) {
-        case .DefaultBlockingCollections(let i):
-            let collection = BlockingCollectionStore.shared.collections.filter{ $0.isDefault }[i]
-            self.pushBlockingCollectionView(collection: collection)
-        case .UserBlockingCollections(let i):
-            let collection = BlockingCollectionStore.shared.collections.filter{ !$0.isDefault }[i]
-            self.pushBlockingCollectionView(collection: collection)
-        case .NewUserBlockingCollection:
-            self.presentCreateBlockingCollectionAlert()
-        case .Schedules:
-            self.pushSchedulesView()
-        case .Settings:
-            self.pushSettingsView()
-        default:
-            break
-        }
-    }
-    
-    
-    /// Conditionally disable selection for some table view rows
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        
-        switch tableViewStructure.rowType(for: indexPath) {
-        case .DefaultBlockingCollections(_), .UserBlockingCollections(_), .NewUserBlockingCollection, .Schedules, .Settings:
-            return indexPath
-        default:
-            return nil
-        }
-    }
-    
-    /// Conditionally enable deletion for user created blocking collections
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        switch tableViewStructure.rowType(for: indexPath) {
-        case .UserBlockingCollections(_): return true
-        default: return false
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        switch (editingStyle, tableViewStructure.rowType(for: indexPath)) {
-        case (.delete, .UserBlockingCollections(let i)):
-            
-            // delete the collection in the data store
-            let collections = BlockingCollectionStore.shared.collections
-            let userCreatedCollections = collections.filter{ $0.isDefault == false }
-            let collectionToDelete = userCreatedCollections[i]
-            BlockingCollectionStore.shared.delete(collectionToDelete)
-            
-            // refresh the table view structure
-            tableViewStructure = updateTableViewStructure()
-
-            // delete the table view cell
-            self.tableView.deleteRows(at: [indexPath], with: .automatic)
-            
-        default: break
-        }
-    }
-    
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView()
-        view.backgroundColor = .clear
-        return view
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        switch tableViewStructure.sectionType(for: section) {
-        case .Schedules, .Settings: return 40
-        default: return 0
         }
     }
 }
